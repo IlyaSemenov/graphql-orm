c16136b75a9b16f24329f304576ae0c1d963bc3c
Ilya Semenov <ilya@semenov.co>
2023-05-23 19:27:36 +0700

    test: decompose setup/apollo from setup/client

--- tests/setup/client.ts
+++ tests/setup/client.ts
@@ -1,41 +1,8 @@
-import { ApolloServer, ApolloServerOptions } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
-import { GraphQLResolveInfo } from "graphql"
 import { GraphQLClient } from "graphql-request"
-import { afterAll } from "vitest"
 
-export interface ResolverContext {
-	user_id: number | null
-}
+import { create_server, ServerConfig } from "./apollo"
 
-export type Resolver<A> = (
-	parent: unknown,
-	args: A,
-	ctx: ResolverContext,
-	info: GraphQLResolveInfo
-) => any
-
-export type Resolvers = Partial<
-	Record<"Query" | "Mutation", Record<string, Resolver<any>>>
->
-
-export type ServerConfig = Required<
-	Pick<ApolloServerOptions<ResolverContext>, "typeDefs" | "resolvers">
->
-
-export async function setup_client(config: ServerConfig) {
-	const server = new ApolloServer<ResolverContext>(config)
-	const { url } = await startStandaloneServer(server, {
-		listen: { port: 0 },
-		async context({ req }) {
-			const user_id = Number(req.headers.user_id) || null
-			return { user_id }
-		},
-	})
-	afterAll(async () => {
-		await server.stop()
-	})
-	const client = new GraphQLClient(url)
-
-	return client
+export async function create_client(config: ServerConfig) {
+	const url = await create_server(config)
+	return new GraphQLClient(url)
 }
--- tests/setup/setup.ts
+++ tests/setup/setup.ts
@@ -1,7 +1,8 @@
-import { ServerConfig, setup_client } from "./client"
+import { ServerConfig } from "./apollo"
+import { create_client } from "./client"
 import { setup_db } from "./db"
 
 export async function setup(config: ServerConfig) {
-	const [client, knex] = await Promise.all([setup_client(config), setup_db()])
+	const [client, knex] = await Promise.all([create_client(config), setup_db()])
 	return { client, knex }
 }
